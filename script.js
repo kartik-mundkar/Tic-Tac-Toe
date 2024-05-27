a92bd45f10797448575a1bdf004c3d40723d15ca
kartik-mundkar <kartikdm2005@gmail.com>
2024-05-27 15:42:52 +0530

    fixed 1st bug(about the player score)

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const player_mode_overlay = document.querySelector(".player-mode-overlay span");
 const audio_overlay = document.querySelector(".audio_overlay");
 const player_score_cards = document.querySelectorAll(".player_score_card");
 const audio_file = document.querySelector("#game_audio");
-const win_imgs=document.querySelectorAll('.win_img')
+const win_imgs = document.querySelectorAll(".win_img");
 
 const mode1_src = "./images/1-player.png";
 const mode2_src = "./images/2-player.png";
@@ -55,203 +55,208 @@ const audio_file_loc = {
 let game_state = 1;
 let is_audio_on = true;
 
-function game_function(){
-    
-function play_audio() {
-    if (is_audio_on) {
-        audio_file.setAttribute("src", audio_file_loc[game_state]);
-        audio_file.play();
-    }
-}
-
-
-function reset_game() {
-    game_state = 1;
-    let player_val;
-    marking_boxes.forEach((box) => {
-        box.textContent = "";
-    });
-    if (game_mode == 1) {
-        player_val = 1;
-        player2_name.innerHTML= "COMPUTER(<span>O</span>)";
-        player_mode_overlay.textContent=2;
-        mode_img.setAttribute("src", mode2_src)
-    } else if (game_mode == 2) {
-        player_val = Math.ceil(Math.random() * 2);
-        player2_name.innerHTML = "PLAYER(<span>O</span>)";
-        player_mode_overlay.textContent=1;
-        mode_img.setAttribute("src", mode1_src)
-    }
-    current_player = `player${player_val}`;
-    // console.log(user_input)
-    current_player_denoters[0].classList.add("hide");
-    current_player_denoters[1].classList.add("hide");
-    win_imgs[0].classList.add('hide');
-    win_imgs[1].classList.add('hide');
-    player_score_cards[0].classList.remove("active_player");
-    player_score_cards[1].classList.remove("active_player");
-    current_player_denoters[player_val - 1].classList.remove("hide");
-    player_score_cards[player_val - 1].classList.add("active_player");
-    user_input.player1 = [];
-    user_input.player2 = [];
-    user_input.not_available = [];
-    player1_score.textContent = match_won.player1;
-    player2_score.textContent = match_won.player2;
-    tie_score.textContent = match_won.tie;
-}
-
-window.addEventListener("load", () => {
-    reset_game();
-});
-
-function toggle_game_mode() {
-    img_src = mode_img.getAttribute("src");
-    if (img_src === mode1_src) {
-        mode_img.setAttribute("src", mode2_src);
-        player2_name.innerHTML = "COMPUTER(<span>O</span>)";
-        game_mode = 1;
-        player_mode_overlay.textContent = "2";
-        reset_game();
-    } else if (img_src === mode2_src) {
-        mode_img.setAttribute("src", mode1_src);
-        player2_name.innerHTML = "PLAYER(<span>O</span>)";
-        game_mode = 2;
-        player_mode_overlay.textContent = "1";
-        reset_game();
-    }
-}
-
-mode_img.addEventListener("click", toggle_game_mode);
-
-audio_box.addEventListener("click", () => {
-    if (audio_box.getAttribute("src") === "./images/mute.png") {
-        audio_box.setAttribute("src", "./images/volume.png");
-        audio_overlay.textContent = "On";
-        is_audio_on = true;
-    } else if (audio_box.getAttribute("src") === "./images/volume.png") {
-        audio_box.setAttribute("src", "./images/mute.png");
-        audio_overlay.textContent = "Off";
-        is_audio_on = false;
-    }
-});
-
-function toggle_current_player() {
-    if (game_state) {
-        if (current_player === "player1") {
-            current_player = "player2";
-            current_player_denoters[0].classList.add("hide");
-            current_player_denoters[1].classList.remove("hide");
-            player_score_cards[0].classList.remove("active_player");
-            player_score_cards[1].classList.add("active_player");
-        } else if (current_player === "player2") {
-            current_player = "player1";
-            current_player_denoters[0].classList.remove("hide");
-            current_player_denoters[1].classList.add("hide");
-            player_score_cards[1].classList.remove("active_player");
-            player_score_cards[0].classList.add("active_player");
+function game_function() {
+    function play_audio() {
+        if (is_audio_on) {
+            audio_file.setAttribute("src", audio_file_loc[game_state]);
+            audio_file.play();
         }
     }
-}
 
-restart.addEventListener("click", reset_game);
+    function reset_game() {
+        game_state = 1;
+        let player_val;
+        marking_boxes.forEach((box) => {
+            box.textContent = "";
+        });
+        if (game_mode == 1) {
+            player_val = 1;
+            player2_name.innerHTML = "COMPUTER(<span>O</span>)";
+            player_mode_overlay.textContent = 2;
+            mode_img.setAttribute("src", mode2_src);
+        } else if (game_mode == 2) {
+            player_val = Math.ceil(Math.random() * 2);
+            player2_name.innerHTML = "PLAYER(<span>O</span>)";
+            player_mode_overlay.textContent = 1;
+            mode_img.setAttribute("src", mode1_src);
+        }
+        current_player = `player${player_val}`;
+        // console.log(user_input)
+        current_player_denoters[0].classList.add("hide");
+        current_player_denoters[1].classList.add("hide");
+        win_imgs[0].classList.add("hide");
+        win_imgs[1].classList.add("hide");
+        player_score_cards[0].classList.remove("active_player");
+        player_score_cards[1].classList.remove("active_player");
+        current_player_denoters[player_val - 1].classList.remove("hide");
+        player_score_cards[player_val - 1].classList.add("active_player");
+        user_input.player1 = [];
+        user_input.player2 = [];
+        user_input.not_available = [];
+        player1_score.textContent = match_won.player1;
+        player2_score.textContent = match_won.player2;
+        tie_score.textContent = match_won.tie;
+    }
 
-function updatescore() {
-    player1_score.textContent = match_won.player1;
-    player2_score.textContent = match_won.player2;
-    tie_score.textContent = match_won.tie;
-}
+    window.addEventListener("load", () => {
+        reset_game();
+    });
 
-marking_boxes.forEach((box, box_index) => {
-    box.addEventListener("click", () => {
+    function toggle_game_mode() {
+        img_src = mode_img.getAttribute("src");
+        if (img_src === mode1_src) {
+            mode_img.setAttribute("src", mode2_src);
+            player2_name.innerHTML = "COMPUTER(<span>O</span>)";
+            game_mode = 1;
+            player_mode_overlay.textContent = "2";
+            match_won.player1 = 0;
+            match_won.player2 = 0;
+            reset_game();
+        } else if (img_src === mode2_src) {
+            mode_img.setAttribute("src", mode1_src);
+            player2_name.innerHTML = "PLAYER(<span>O</span>)";
+            game_mode = 2;
+            player_mode_overlay.textContent = "1";
+            match_won.player1 = 0;
+            match_won.player2 = 0;
+            reset_game();
+        }
+    }
+
+    mode_img.addEventListener("click", toggle_game_mode);
+
+    audio_box.addEventListener("click", () => {
+        if (audio_box.getAttribute("src") === "./images/mute.png") {
+            audio_box.setAttribute("src", "./images/volume.png");
+            audio_overlay.textContent = "On";
+            is_audio_on = true;
+        } else if (audio_box.getAttribute("src") === "./images/volume.png") {
+            audio_box.setAttribute("src", "./images/mute.png");
+            audio_overlay.textContent = "Off";
+            is_audio_on = false;
+        }
+    });
+
+    function toggle_current_player() {
         if (game_state) {
-            if (marking_boxes[box_index].textContent === "") {
-                marking_boxes[box_index].textContent =
-                    user_label[current_player];
-                user_input[current_player].push(box_index);
-                user_input.not_available.push(box_index);
-                if (win_check()) {
-                    updatescore();
-                    setTimeout(reset_game, 1500);
-                    console.log("winner is declared");
-                    return;
-                } else {
-                    play_audio();
-                    // audio_file.play();
-                    toggle_current_player();
-                }
+            if (current_player === "player1") {
+                current_player = "player2";
+                current_player_denoters[0].classList.add("hide");
+                current_player_denoters[1].classList.remove("hide");
+                player_score_cards[0].classList.remove("active_player");
+                player_score_cards[1].classList.add("active_player");
+            } else if (current_player === "player2") {
+                current_player = "player1";
+                current_player_denoters[0].classList.remove("hide");
+                current_player_denoters[1].classList.add("hide");
+                player_score_cards[1].classList.remove("active_player");
+                player_score_cards[0].classList.add("active_player");
+            }
+        }
+    }
 
-                if (
-                    game_mode == 1 &&
-                    current_player == "player2" &&
-                    game_state
-                ) {
-                    while (user_input.not_available.length != 9) {
-                        //this is computers choice you have to make it better so that you can have a better game
-                        computer_choice = Math.floor(Math.random() * 9);
+    restart.addEventListener("click", reset_game);
 
-                        if (marking_boxes[computer_choice].textContent === "") {
-                            setTimeout(() => {
-                                marking_boxes[computer_choice].textContent =
-                                    user_label[current_player];
-                                user_input[current_player].push(
-                                    computer_choice
-                                );
-                                user_input.not_available.push(computer_choice);
-                                if (win_check() != 0) {
-                                    updatescore();
-                                    setTimeout(reset_game, 1500);
-                                    // console.log("winner is declared");
-                                    return;
-                                } else {
-                                    play_audio();
-                                    // audio_file.play();
-                                    toggle_current_player();
-                                }
-                            }, 500);
-                            break;
+    function updatescore() {
+        player1_score.textContent = match_won.player1;
+        player2_score.textContent = match_won.player2;
+        tie_score.textContent = match_won.tie;
+    }
+
+    marking_boxes.forEach((box, box_index) => {
+        box.addEventListener("click", () => {
+            if (game_state) {
+                if (marking_boxes[box_index].textContent === "") {
+                    marking_boxes[box_index].textContent =
+                        user_label[current_player];
+                    user_input[current_player].push(box_index);
+                    user_input.not_available.push(box_index);
+                    if (win_check()) {
+                        updatescore();
+                        setTimeout(reset_game, 1500);
+                        console.log("winner is declared");
+                        return;
+                    } else {
+                        play_audio();
+                        // audio_file.play();
+                        toggle_current_player();
+                    }
+
+                    if (
+                        game_mode == 1 &&
+                        current_player == "player2" &&
+                        game_state
+                    ) {
+                        while (user_input.not_available.length != 9) {
+                            //this is computers choice you have to make it better so that you can have a better game
+                            computer_choice = Math.floor(Math.random() * 9);
+
+                            if (
+                                marking_boxes[computer_choice].textContent ===
+                                ""
+                            ) {
+                                setTimeout(() => {
+                                    marking_boxes[computer_choice].textContent =
+                                        user_label[current_player];
+                                    user_input[current_player].push(
+                                        computer_choice
+                                    );
+                                    user_input.not_available.push(
+                                        computer_choice
+                                    );
+                                    if (win_check() != 0) {
+                                        updatescore();
+                                        setTimeout(reset_game, 1500);
+                                        // console.log("winner is declared");
+                                        return;
+                                    } else {
+                                        play_audio();
+                                        // audio_file.play();
+                                        toggle_current_player();
+                                    }
+                                }, 500);
+                                break;
+                            }
                         }
                     }
                 }
-            }
 
-            // this is for draw matches
-            if (user_input.not_available.length == 9 && game_state) {
-                match_won.tie += 1;
+                // this is for draw matches
+                if (user_input.not_available.length == 9 && game_state) {
+                    match_won.tie += 1;
+                    setTimeout(reset_game, 1500);
+                    return 0;
+                }
+            }
+            if (!game_state) {
+                updatescore();
                 setTimeout(reset_game, 1500);
-                return 0;
-            }
-        }
-        if (!game_state) {
-            updatescore();
-            setTimeout(reset_game, 1500);
-        }
-    });
-});
-
-function win_check() {
-    winning_seq.forEach((seq) => {
-        let seq_match_count = 0;
-        seq.forEach((index) => {
-            if (user_input[current_player].includes(index)) {
-                seq_match_count += 1;
             }
         });
-        if (seq_match_count == 3) {
-            match_won[current_player] += 1;
-            game_state = 0;
-            if(current_player=='player1'){
-                win_imgs[0].classList.remove('hide')
-            }
-            else if(current_player=='player2'){
-                win_imgs[1].classList.remove('hide')
-            }
-            return current_player == "player1" ? 1 : 2;
-        }
     });
 
-    return 0;
-}
+    function win_check() {
+        winning_seq.forEach((seq) => {
+            let seq_match_count = 0;
+            seq.forEach((index) => {
+                if (user_input[current_player].includes(index)) {
+                    seq_match_count += 1;
+                }
+            });
+            if (seq_match_count == 3) {
+                match_won[current_player] += 1;
+                game_state = 0;
+                if (current_player == "player1") {
+                    win_imgs[0].classList.remove("hide");
+                } else if (current_player == "player2") {
+                    win_imgs[1].classList.remove("hide");
+                }
+                return current_player == "player1" ? 1 : 2;
+            }
+        });
 
+        return 0;
+    }
 }
 
 game_function();
